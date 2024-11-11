59cebb9a51448102b787e970cf6f412326f5727d
Madhav Kabra <madhav.kabra730@gmail.com>
2024-11-11 14:59:27 +0530

    Fixed mocked server handler

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,9 +1,45 @@
 // src/mocks/handlers.js
-import { http, HttpResponse } from 'msw'
-import  data  from './data.json'
+import { http, HttpResponse } from "msw";
+const data = [
+  {
+    type: "bank-draft",
+    title: "Bank Draft",
+    position: 0,
+    thumbnail:
+      "https://images.unsplash.com/photo-1478098711619-5ab0b478d6e6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2F0fGVufDB8fDB8fHww",
+  },
+  {
+    type: "bill-of-lading",
+    title: "Bill of Lading",
+    position: 1,
+    thumbnail:
+      "https://images.unsplash.com/photo-1573865526739-10659fec78a5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNhdHxlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    type: "invoice",
+    title: "Invoice",
+    position: 2,
+    thumbnail:
+      "https://images.unsplash.com/photo-1513360371669-4adf3dd7dff8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNhdHxlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    type: "bank-draft-2",
+    title: "Bank Draft 2",
+    position: 3,
+    thumbnail:
+      "https://plus.unsplash.com/premium_photo-1677545182067-26ac518ef64f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Y2F0fGVufDB8fDB8fHww",
+  },
+  {
+    type: "bill-of-lading-2",
+    title: "Bill of Lading 2",
+    position: 4,
+    thumbnail:
+      "https://plus.unsplash.com/premium_photo-1664299749481-ac8dc8b49754?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGNhdHxlbnwwfHwwfHx8MA%3D%3D",
+  },
+];
 export const handlers = [
   // Intercept "GET documents" requests...
-  http.get('/documents', () => {
-    return HttpResponse.json(data.documents)
+  http.get("/documents", () => {
+    return HttpResponse.json(data);
   }),
-]
+];
